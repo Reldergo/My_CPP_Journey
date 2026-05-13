9c7bc48eaae19fed2064a3944918e4003fc759ec
Rafid Chowdhury <rafidchowdhury.dev@gmail.com>
2026-05-13 19:56:41 +0600

    Add files via upload
    Solved the functions problem from HackerRank.

---
+++ Functions.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+
+int max_of_four(int a, int b, int c, int d);
+
+int main(){
+
+    int a, b, c, d;
+
+    std::cin >> a;
+
+    std::cin >> b;
+
+    std::cin >> c;
+
+    std::cin >> d;
+
+    int z = max_of_four(a, b, c, d);
+
+    std::cout << z;
+
+    return 0;
+
+}
+
+    int max_of_four(int a, int b, int c, int d){
+
+    return std::max(std::max(a, b), std::max(c, d));
+
+    }
