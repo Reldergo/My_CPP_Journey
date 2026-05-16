05152f872ec40c18438cb4a0a451349778aeb884
Rafid Chowdhury <rafidchowdhury.dev@gmail.com>
2026-05-16 16:32:05 +0600

    Add files via upload
    Just solved variable sized array problem from HackerRank.

---
+++ VariableSizedArrays.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main(){
+
+    int n, q;
+
+    std::vector<std::vector<int>> main_arr;
+
+    std::cin >> n;
+
+    std::cin >> q;
+
+    for(int i = 0; i < n; i++){
+
+        int s;
+
+        std::cin >> s;
+
+        std::vector <int> arr (s);
+
+        for(int j = 0; j < s; j++){
+
+            std::cin >> arr[j];
+
+        }
+
+        main_arr.push_back(arr);
+
+    }
+
+    for(int f = 0; f < q; f++){
+
+        int num, index;
+
+        std::cin >> num;
+
+        std::cin >> index;
+
+        std::cout << main_arr[num][index] << std::endl;
+
+
+    }
+
+    return 0;
+
+}
